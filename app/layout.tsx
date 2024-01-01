04ba6c591035cc754c4459dcf2bf265eea19fbe3
Serhii <slobojko21@gmail.com>
2024-01-01 20:00:45 +0200

    follow service

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -21,7 +22,14 @@ export default function RootLayout({
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider attribute='class' forcedTheme='dark' storageKey='gamehub-theme'>{children}</ThemeProvider>
+          <ThemeProvider
+            attribute="class"
+            forcedTheme="dark"
+            storageKey="gamehub-theme"
+          >
+            <Toaster theme="light" position="bottom-center" />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
