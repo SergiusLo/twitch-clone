1f6f00d9ef01c54ae31f3705504cff0c6e1e40d0
Serhii <slobojko21@gmail.com>
2024-02-21 12:58:28 +0200

    about-card

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -7,12 +7,13 @@ import { LiveKitRoom } from "@livekit/components-react";
 import { Stream, User } from "@prisma/client";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
+import { Header, HeaderSkeleton } from "./header";
+import { InfoCard } from "./info-card";
 import { Video, VideoSkeleton } from "./video";
-import { Header, HeaderSkeleton } from './header';
-import { InfoCard } from './info-card';
+import { AboutCard } from './about-card';
 
 interface StreamPlayerProps {
-  user: User & { stream: Stream | null };
+  user: User & { stream: Stream | null; _count: { followedBy: number } };
   stream: Stream;
   isFollowing: boolean;
 }
@@ -55,7 +56,19 @@ export const StreamPlayer = ({
             imageUrl={user.imageUrl}
             name={stream.name}
           />
-          <InfoCard hostIdentity={user.id} viewerIdentity={identity} name={stream.name} thumbnaiUrl={stream.thumbnaiUrl}/>
+          <InfoCard
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            name={stream.name}
+            thumbnaiUrl={stream.thumbnaiUrl}
+          />
+          <AboutCard
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            bio={user.bio}
+            followedByCount={user._count.followedBy}
+          />
         </div>
         <div className={cn("col-span-1", collapsed && "hidden")}>
           <Chat
@@ -78,7 +91,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
-        <HeaderSkeleton/>
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
